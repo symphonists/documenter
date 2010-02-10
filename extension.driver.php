ad72419e5be21754c8caf03c1fec6ac32b9ecd44
craig zheng <cz@mongrl.com>
2010-02-10 14:12:07 -0500

    Update version

--- extension.driver.php
+++ extension.driver.php
@@ -5,8 +5,8 @@
 		public function about() {
 			return array(
 				'name'			=> 'Documenter',
-				'version'		=> '0.9.6',
-				'release-date'	=> '2009-01-31',
+				'version'		=> '0.9.7',
+				'release-date'	=> '2009-02-10',
 				'author'		=> array(
 					'name'			=> 'craig zheng',
 					'email'			=> 'craig@symphony-cms.com'
