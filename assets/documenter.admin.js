c4eac889e118eb3673f27208f1071c942a00efca
Nils Hörrmann <post@nilshoerrmann.de>
2011-04-13 14:56:25 +0200

    Update button implementation, adjust documenter interface when system messages are loaded (static and live), general clean-up

--- assets/documenter.admin.js
+++ assets/documenter.admin.js
@@ -13,61 +13,102 @@
 			title = $('#documenter-title'),
 			button = $('a.documenter.button'),
 			help = button.text(),
-			docs = $('#documenter-drawer'),
-			notice = $('#notice');
-			
-		// Check for system messages
-		if(notice.is(':visible')) {
-			docs.addClass('notice');
-		}
+			drawer = $('#documenter-drawer');
+								
+	/*---- Events -----------------------------------------------------------*/
 		
 		// Toggle documentation
 		button.click(function(event) {
 			
 			// Hide documentation
 			if(button.is('.active')) {
-				docs.animate({
-					width: 0,
-					overflow: 'hidden'
-				}, 'fast');
-				
-				// Store state
-				wrapper.removeClass('documenter');
-				button.text(help).attr('title', Symphony.Language.get('View Documentation')).removeClass('active');
-				if(localStorage) {
-					localStorage.removeItem('documenter-' + Symphony.Context.get('root'));
-				}
+				hide('fast');
 			}
 			
 			// Show documentation
 			else {
-				docs.animate({
-					width: 300,
-					overflow: 'auto'
-				}, 'fast');	
-				
-				// Store state
-				wrapper.addClass('documenter');
-				button.text('×').attr('title', Symphony.Language.get('Hide Documentation')).addClass('active');
-				if(localStorage) {
-					localStorage.setItem('documenter-' + Symphony.Context.get('root'), 'active');
-				}
+				show('fast');
 			}
 		});
+						
+		// Detect live notices
+		$('#header').bind('DOMSubtreeModified', function() {
+			notice();
+		});
+		$(window).bind('resize', function() {
+			notice();
+		});
 		
-		// Restore documentation state
+	/*---- Functions --------------------------------------------------------*/
+		
+		// Show documentation
+		var show = function(speed) {
+			button.fadeOut('fast');
+			
+			// Open drawer
+			drawer.animate({
+				width: 300,
+				overflow: 'auto'
+			}, speed, function() {
+				wrapper.addClass('documenter');
+				button.text('×').attr('title', Symphony.Language.get('Hide Documentation')).addClass('active').fadeIn('fast');			
+			});	
+			
+			// Store state
+			if(localStorage) {
+				localStorage.setItem('documenter-' + Symphony.Context.get('root'), 'active');
+			}
+		};
+		
+		// Hide documentation
+		var hide = function(speed) {
+			notice();
+		
+			// Close drawer
+			drawer.animate({
+				width: 0,
+				overflow: 'hidden'
+			}, 'fast', function() {
+				wrapper.removeClass('documenter');
+				button.text(help).attr('title', Symphony.Language.get('View Documentation')).removeClass('active');
+			});
+			
+			// Store state
+			if(localStorage) {
+				localStorage.removeItem('documenter-' + Symphony.Context.get('root'));
+			}
+		};
+		
+		// Check notices
+		var notice = function() {
+			var message = $('#notice'),
+				height = message.outerHeight();
+				
+			// System messages active
+			if($('#notice').is(':visible')) {
+				button.css('top', 16 + height);
+				drawer.css('padding-top', height);
+			}
+			
+			// System messages inactive
+			else {
+				button.css('top', 16);
+				drawer.css('padding-top', 0);
+			}
+		}
+			
+	/*---- Initialisation ---------------------------------------------------*/
+		
+		// Check for existing system messages
+		notice();
+		
+		// Restore documenter state
 		if(localStorage) {
 			if(localStorage.getItem('documenter-' + Symphony.Context.get('root')) == 'active') {
-				docs.css({
-					width: 300,
-					overflow: 'auto'
-				});
-				
-				// Store state
-				wrapper.addClass('documenter');
-				button.text('×').attr('title', Symphony.Language.get('Hide Documentation')).addClass('active');
+				show(0);
 			}
-		}	
+		}
+		
 	});
 
 })(jQuery.noConflict());	
