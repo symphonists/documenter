91ddc62b792cacc9fcb56dc34268db21c6994b9d
Nils Hörrmann <post@nilshoerrmann.de>
2012-07-30 20:06:05 +0200

    Update German translation (incomplete)

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,9 +7,8 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://www.nilshoerrmann.de'
 		),
-		'release-date' => '2010-05-13',
+		'release-date' => '2012-07-30'
 	);
-
 
 	/**
 	 * Documenter
@@ -18,9 +17,6 @@
 
 		'Button Text' => 
 		'Schaltflächentext',
-
-		'Content' => 
-		'Inhalt',
 
 		'Content is a required field' => 
 		'Inhalt ist ein Pflichtfeld',
@@ -31,20 +27,14 @@
 		'Delete this template' => 
 		'Diese Vorlage löschen',
 
-		'Documentation' => 
-		'Dokumentation',
-
-		'Documentation Item' => 
-		'Dokumentationseintrag',
-
-		'Documentation Item not found' => 
-		'Dokumentationseintrag konnte nicht gefunden werden',
-
 		'Document It' => 
 		'Dokumentieren',
 
-		'Help' => 
-		'Hilfe',
+		'Documentation' => 
+		'Dokumentation',
+
+		'Documentation Item not found' => 
+		'Dokumentationseintrag konnte nicht gefunden werden',
 
 		'Page is a required field' => 
 		'Seite ist ein Pflichtfeld',
@@ -55,10 +45,25 @@
 		'The documentation item you requested to edit does not exist.' => 
 		'Der aufgerufene Dokumentationseintrag existiert nicht. ',
 
-		'View Documentation' => 
-		'Dokumentation einblenden',
-		
-		'Hide Documentation' =>
-		'Dokumentation ausblenden'
+		'Show' => 
+		'anzeigen',
+
+		'Hide' => 
+		'schließen',
+
+		'Unknown errors occurred while attempting to save. Please check your <a href="%s">activity log</a>.' => 
+		false,
+
+		'An error occurred while processing this form. <a href="#error">See below for details.</a>' => 
+		false,
+
+		'Title is a required field' => 
+		false,
+
+		'Documentation Item updated at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Documentation</a>' => 
+		false,
+
+		'Documentation Item created at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Documentation</a>' => 
+		false,
 
 	);
